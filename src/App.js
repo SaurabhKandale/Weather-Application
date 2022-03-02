6a1aab7c4983c9009606166b8cbb17dc852853d7
Saurabh <saurabhkandale07@gmail.com>
2022-03-02 13:05:48 +0530

    Deployed

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import DisplayData from "./Components/DisplayData";
 
 function App() {
 
-  const [name,setname]=useState("Enter City Name");
+  const [name,setname]=useState("Enter Location");
   const [value,setValue]=useState(false);
 
   return (
     <div className="App">
-      <div className="flex justify-center items-center h-screen w-full p-4 xl:px-64 xl:py-20 2xl:px-80 2xl:py-36">
+      <div className="flex justify-center items-center h-full w-full px-4 py-6 xl:px-64 xl:py-20 2xl:px-80 2xl:py-36">
         <div className="bg-violet-100/50 border-2 border-violet-900 rounded-2xl shadow-2xl grid grid-cols-3 min-h-full min-w-full gap-0 md:gap-3 gap-y-0">
           <div className="col-span-2 flex justify-center items-center h-36 px-3 md:px-0">
-          <input className="w-full md:w-9/12 bg-transparent border-b-2 border-violet-900 py-1 px-2 focus:outline-0 text-3xl transition-all text-violet-900 font-bold"  type="text" value={name} onChange={(e)=>{setname(e.target.value);setValue(false)}} />
+          <input className="w-full md:w-9/12 bg-transparent border-b-2 border-violet-900 py-1 px-2 focus:outline-0 text-xl md:text-3xl transition-all text-violet-900 font-bold"  type="text" value={name} onChange={(e)=>{setname(e.target.value);setValue(false)}} />
           </div>
           <div className="col-span-1 h-36 flex justify-center items-center px-3 md:px-0">
-          <button className="border-2 border-violet-900 text-violet-900 w-full md:w-9/12 lg:w-7/12 py-1 rounded-xl shadow-xl text-lg transition-all hover:text-violet-100 hover:bg-violet-800 font-bold px-2" onClick={()=>{setValue(true)}}>Search</button>
+          <button className="border-2 border-violet-900 text-violet-900 w-full md:w-9/12 lg:w-7/12 py-1 rounded-xl shadow-xl text-md md:text-lg transition-all hover:text-violet-100 hover:bg-violet-800 font-bold px-2" onClick={()=>{setValue(true)}}>Search</button>
           </div>
-          <div className="col-span-3 text-center md:h-64">
+          <div className="py-4 col-span-3 text-center md:h-72">
           {value && <DisplayData cityName={name} />}
         </div>
         </div>
--- src/Components/DisplayData.js
+++ src/Components/DisplayData.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { connect, useDispatch } from "react-redux";
 import callApi from "../Redux/call";
 import { emptyAction } from "../Redux/Actions/Actions";
-
+import clouds from "../Images/clouds.png"
+import sunny from "../Images/day.png"
+import clear from "../Images/good-weather.png"
+import haze from "../Images/haze.png"
+import rainy from "../Images/rainy.png"
+import smoke from "../Images/smoke.png"
+import drizzle from "../Images/drizzle.png"
+import snow from "../Images/snow.png"
 const DisplayData = (props) => {
 
-    console.log(props.cityName);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -18,30 +24,27 @@ const DisplayData = (props) => {
 
     if (props.userData && props.userData.city.main) {
 
-        var link="https://cdn-icons.flaticon.com/png/512/2280/premium/2280705.png?token=exp=1646156146~hmac=3b7b10f009ddc0b8f22b44692bc5a95a"
-        if(props.userData.city.weather[0].main=="Clouds")
+        var link=clear;
+        switch(props.userData.city.weather[0].main)
         {
-            link="https://cdn-icons-png.flaticon.com/512/414/414825.png";
+            case "Clouds":link=clouds;break;
+            case "Sunny":link=sunny;break;
+            case "Haze":link=haze;break;
+            case "Rain":link=rainy;break;
+            case "Smoke":link=smoke;break;
+            case "Drizzle":link=drizzle;break;
+            case "Snow":link=snow;break;
+
+            default : link=clear;
         }
-        if(props.userData.city.weather[0].main=="Haze")
-        {
-            link="https://cdn-icons-png.flaticon.com/512/1779/1779807.png";
-        }
-        if(props.userData.city.weather[0].main=="Rain")
-        {
-            link="https://cdn-icons.flaticon.com/png/512/2337/premium/2337478.png?token=exp=1646156795~hmac=98241266b4132a7ae1b3be62da5a861c";
-        }
-        if(props.userData.city.weather[0].main=="Smoke")
-        {
-            link="https://cdn-icons.flaticon.com/png/512/4429/premium/4429947.png?token=exp=1646157503~hmac=dac4ea8bfab337565ba082cebbbb832f";
-        }
+        
 
         return (
             <div className="flex justify-around items-center flex-col md:flex-row ">
                 <div>
-                    <img src={link} alt="clouds" className="h-24 md:h-56 object-fit" />
+                    <img src={link} alt={props.userData.city.weather[0].main} className="mt-5 md:mt-0 h-36 md:h-56 object-fit" />
                 </div>
-                <div className="font-bold mt-5 md:mt-0">
+                <div className=" py-8 md:py-0 font-bold mt-2 md:mt-0">
                     <span className="block text-4xl md:text-6xl py-6">{props.userData.city.name}</span>
                     <span className="block text-xl">{props.userData.city.weather[0].main}</span>
                     <span className="block text-xl py-2"><span className="px-2">Min. {(props.userData.city.main.temp_min - 273.15).toFixed(2)}&#176;C</span> || <span className="px-2">Max. {(props.userData.city.main.temp_max - 273.15).toFixed(2)}&#176;C</span></span>
